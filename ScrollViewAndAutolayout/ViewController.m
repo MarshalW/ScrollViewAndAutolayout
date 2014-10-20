2e015c9a2c8495358e89486c1d3800835e931253
Marshal <marshal.wu@qq.com>
2014-10-20 20:32:20 +0800

    实现了纯autolayout方式的示例

--- ScrollViewAndAutolayout/ViewController.m
+++ ScrollViewAndAutolayout/ViewController.m
@@ -14,6 +14,10 @@
 
 @implementation ViewController{
     __weak IBOutlet UIScrollView *scrollView;
+//    __weak IBOutlet UIView *outerView;
+    UIView *contentView;
+    
+    NSLayoutConstraint *widthConstraint;
 }
 
 - (void)viewDidLoad {
@@ -21,11 +25,76 @@
     [self initContentView];
 }
 
+-(void)viewDidLayoutSubviews{
+    NSLog(@"view did layout subviews");
+    widthConstraint.constant=scrollView.frame.size.width;
+    
+    //为了兼容iOS7，http://stackoverflow.com/questions/15490140/auto-layout-error
+    //iOS8下无需这句话
+    [self.view layoutSubviews];
+}
+
 -(void)initContentView{
-    UIView *contentView=[[[NSBundle mainBundle] loadNibNamed:@"ContentView" owner:self options:nil] objectAtIndex:0];
-    contentView.frame=CGRectMake(0, 0, scrollView.bounds.size.width, contentView.bounds.size.height);
+    contentView=[[[NSBundle mainBundle] loadNibNamed:@"ContentView" owner:self options:nil] objectAtIndex:0];
     [scrollView addSubview:contentView];
-    scrollView.contentSize=contentView.bounds.size;
+    
+    scrollView.translatesAutoresizingMaskIntoConstraints=NO;
+    contentView.translatesAutoresizingMaskIntoConstraints=NO;
+    
+    //top
+    [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:contentView
+                                                           attribute:NSLayoutAttributeTop
+                                                           relatedBy:NSLayoutRelationEqual
+                                                              toItem:scrollView
+                                                           attribute:NSLayoutAttributeTop
+                                                          multiplier:1.0
+                                                            constant:0]];
+    
+    //bottom
+    [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:contentView
+                                                           attribute:NSLayoutAttributeBottom
+                                                           relatedBy:NSLayoutRelationEqual
+                                                              toItem:scrollView
+                                                           attribute:NSLayoutAttributeBottom
+                                                          multiplier:1.0
+                                                            constant:0]];
+    
+    //left
+    [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:contentView
+                                                           attribute:NSLayoutAttributeLeading
+                                                           relatedBy:NSLayoutRelationEqual
+                                                              toItem:scrollView
+                                                           attribute:NSLayoutAttributeLeading
+                                                          multiplier:1.0
+                                                            constant:0]];
+    
+    //right
+    [scrollView addConstraint:[NSLayoutConstraint constraintWithItem:contentView
+                                                           attribute:NSLayoutAttributeTrailing
+                                                           relatedBy:NSLayoutRelationEqual
+                                                              toItem:scrollView
+                                                           attribute:NSLayoutAttributeTrailing
+                                                          multiplier:1.0
+                                                            constant:0]];
+    
+    //content.width
+    widthConstraint=[NSLayoutConstraint constraintWithItem:contentView
+                                                 attribute:NSLayoutAttributeWidth
+                                                 relatedBy:NSLayoutRelationEqual
+                                                    toItem:nil
+                                                 attribute:NSLayoutAttributeNotAnAttribute
+                                                multiplier:1.0
+                                                  constant:scrollView.frame.size.width];
+    [contentView addConstraint:widthConstraint];
+    
+    //content.height
+    [contentView addConstraint:[NSLayoutConstraint constraintWithItem:contentView
+                                                            attribute:NSLayoutAttributeHeight
+                                                            relatedBy:NSLayoutRelationEqual
+                                                               toItem:nil
+                                                            attribute:NSLayoutAttributeNotAnAttribute
+                                                           multiplier:1.0
+                                                             constant:contentView.frame.size.height]];
 }
 
 @end
